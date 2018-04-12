d6ec8e6ffeb5d3f485bed7a0aa4abd4e3d9f5643
James Zhao <jamzhao@gmail.com>
2018-04-12 15:54:42 +0800

    Add second USB Device bypass to Qemu

--- support/scripts/run_qemu_ath10k.sh
+++ support/scripts/run_qemu_ath10k.sh
@@ -6,14 +6,16 @@ BR_TOP_DIR=`readlink -f $SCRIPT_DIR/../..`
 ZIMAGE=$BR_TOP_DIR/output/images/bzImage
 INITRD=$BR_TOP_DIR/output/images/rootfs.cpio
 QEMU=qemu-system-x86_64
-USB_DEVICE=vendorid=0x13b1,productid=0x0042
+USB_DEVICE1=vendorid=0x13b1,productid=0x0042
+USB_DEVICE2=vendorid=0x0cf3,productid=0x9271
 
 ${QEMU} \
 	-kernel ${ZIMAGE}					\
 	-initrd ${INITRD}					\
 	-usb								\
 	-device usb-ehci,id=ehci			\
-	-device usb-host,${USB_DEVICE}		\
+	-device usb-host,${USB_DEVICE1}		\
+	-device usb-host,${USB_DEVICE2}		\
 	-enable-kvm							\
 	-append "console=tty1 rw"			\
 	-nographic							\
